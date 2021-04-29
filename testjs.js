0477e220878a6b5190c52a6f9ad9d68b088f6318
anshparmar <83338180+anshparmar@users.noreply.github.com>
2021-04-29 10:40:47 +0530

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,81 +1,113 @@
 var seq=[];
-var i;
-
-function check( i )
-{
-  $("#a1").click(function()
-{
-  if(seq[i]==1)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-});
-$("#a2").click(function()
-{
-if(seq[i]==2)
-{
-  return 1;
-}
-else
-{
-  return 0;
-}
-});
-$("#a3").click(function()
-{
-if(seq[i]==3)
-{
-  return 1;
-}
-else
-{
-  return 0;
-}
-});
-$("#a4").click(function()
-{
-if(seq[i]==4)
-{
-  return 1;
-}
-else
-{
-  return 0;
-}
-});
-}
-
-
+var i,level=1;
 $(".start").click(function()
 {
-  var level=1;
-  var k=1;
-
+i=0;
+var x=Math.floor(Math.random()*10)%4+1;
+$("#a"+x).fadeIn(100).fadeOut(100).fadeIn(100);
+seq.push(x);
+console.log(seq);
+$(".start").text("Level"+level);
+  $("#a1").click(function()
   {
-    var x=Math.floor(Math.random()*10)%4+1;
-    seq.push(x);
-    var add="a"+x;
-    $(add).click();
-    for(i=0;i<seq.length;i++)
-    while(k==1)
+    if(seq[i]==1)
     {
-      if(check(i)==0)
-      {
-        k=0;
-        break;
-      }
+      i++;
     }
-    level++;
+    else
+    {
+      alert("YOU LEVEL LOSER BITCH! Could only pass Level "+(level-1) );
+      seq=[];
+      level=0;
+      $(".start").text("Restart");
+    }
+    if(i==seq.length)
+    {
+      i=0;
+      var x=Math.floor(Math.random()*10)%4+1;
+      seq.push(x);
+      console.log(seq);
+      level++;
+      $(".start").text("Level"+level);
+      $("#a"+x).fadeIn(100).fadeOut(100).fadeIn(100);
+    }
+  });
 
+  $("#a2").click(function()
+  {
+    if(seq[i]==2)
+    {
+      i++;
+    }
+    else
+    {
+  alert("YOU LEVEL LOSER BITCH! Could only pass Level "+(level-1) );
+      seq=[];
+      level=0;
+      $(".start").text("Restart");
+    }
+    if(i==seq.length)
+    {
+      i=0;
+      var x=Math.floor(Math.random()*4)+1;
+      seq.push(x);
+      console.log(seq);
+      level++;
+      $(".start").text("Level"+level);
+      $("#a"+x).fadeIn(100).fadeOut(100).fadeIn(100);
+    }
+  });
 
-  }
+  $("#a3").click(function()
+  {
+    if(seq[i]==3)
+    {
+      i++;
+    }
+    else
+    {
+    alert("YOU LEVEL LOSER BITCH! Could only pass Level "+(level-1) );
+      seq=[];
+      level=0;
+      $(".start").text("Restart");
+    }
+    if(i==seq.length)
+    {
+      i=0;
+      var x=Math.floor(Math.random()*10)%4+1;
+      seq.push(x);
+      console.log(seq);
+      level++;
+      $(".start").text("Level"+level);
+      $("#a"+x).fadeIn(100).fadeOut(100).fadeIn(100);
 
+    }
+  });
 
-
+  $("#a4").click(function()
+  {
+    if(seq[i]==4)
+    {
+      i++;
+    }
+    else
+    {
+      alert("YOU LEVEL LOSER BITCH! Could only pass Level "+(level-1) );
+      seq=[];
+      level=0;
+      $(".start").text("Restart");
+    }
+    if(i==seq.length)
+    {
+      i=0;
+      var x=Math.floor(Math.random()*10)%4+1;
+      seq.push(x);
+      console.log(seq);
+      level++;
+      $(".start").text("Level"+level);
+      $("#a"+x).fadeOut(100).fadeIn(100);
+    }
+  });
 
 
 
